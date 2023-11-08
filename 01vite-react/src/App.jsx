88d7ba5a054990495e3e5207cffd386efd48218f
loki7349 <lokeshargon19l@gmail.com>
2023-11-08 12:25:37 +0530

    customreact

--- 01vite-react/src/App.jsx
+++ 01vite-react/src/App.jsx
@@ -1,13 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import Chai from "./chai"
 function App() {
- 
+const user = "It's Loki"
 
   return (
       <>
       <Chai/>
-      <h1>Hello Preet</h1>
-      <p><b>Hi, Sonu</b></p>
+      <h1>Hello Preet {user}</h1>
+      <p><b>Hi, Sonu {user}</b></p>
       </>
   )
 }
--- 01vite-react/src/main.jsx
+++ 01vite-react/src/main.jsx
@@ -1,11 +1,28 @@
-// import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return(
+        <>
+        <h1>Visit me</h1>
+        </>
+    )
+}
 
+const anotherElement = (
+    <a href="https://google.com" target='_blank' rel="noreferrer">visit google </a>
+)
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'Click me to visit google'
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
+    // reactElement
     <App />
-  
+
 )
