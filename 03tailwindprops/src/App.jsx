323c2edb67248d38f978e827550d3a7497bc43c5
loki7349 <lokeshargon19l@gmail.com>
2023-11-16 23:58:55 +0530

    props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let myObj = {
+    username: 'Loki',
+    age: 23
+  }
+
+  let newArr = [1,4,3,4,5];
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+
+      {/* <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://images.pexels.com/photos/19065036/pexels-photo-19065036/free-photo-of-anis.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" width="384" height="512"/>
+  <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p class="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption class="font-medium">
+      <div class="text-sky-500 dark:text-sky-400">
+        Sarah Dayan
+      </div>
+      <div class="text-slate-700 dark:text-slate-500">
+        Staff Engineer, Algolia
+      </div>
+    </figcaption>
+  </div>
+</figure> */}
+
+{/* <div class="relative h-[400px] w-[300px] rounded-md">
+  <img
+    src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+    alt="AirMax Pro"
+    class="z-0 h-full w-full rounded-md object-cover"
+  />
+  <div class="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+  <div class="absolute bottom-4 left-4 text-left">
+    <h1 class="text-lg font-semibold text-white">Delba</h1>
+    <p class="mt-2 text-sm text-gray-300">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      debitis?
+    </p>
+    <button class="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-black">
+      View Profile →
+    </button>
+  </div>
+</div> */}
+
+
+      <Card username="Loki with sonu" btnText="click me"/>
+      <Card username="Loki with bandaria" />
+
+
+    </>
+  )
+}
+
+export default App
