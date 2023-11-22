318710a13c1ead64be3ff4a0d6bd72af139ab503
loki7349 <lokeshargon19l@gmail.com>
2023-11-22 23:45:21 +0530

    passwordGenerator

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -6,9 +6,8 @@ function App() {
 
   return (
     <>
-      <div className="w-full h-screen duration-200" 
-      style={{backgroundColor: color}}
-      >
+      <div className="w-full h-screen duration-200"
+      style={{backgroundColor: color}}>
         <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
           <div className='flex flex-wrap justify-content gap-3 shadow-lg bg-white  px-3 py-3 rounded-3xl'>
             <button onClick={() => setColor("red")}
