e88a8d7552bfd7229ce5412a92f40317a7498205
loki7349 <lokeshargon19l@gmail.com>
2024-01-06 22:26:52 +0530

    Added the React Hook form in React project

--- 12MegaBlog/src/components/container/Container.jsx
+++ 12MegaBlog/src/components/container/Container.jsx
@@ -2,8 +2,11 @@
 import React, { Children } from "react";
 
 function Container() {
-    return <div className="w-full max-w-7xl mx-auto px-4">
-        {Children}</div>;
+    return (
+        <div className="w-full max-w-7xl mx-auto px-4">
+            {Children}
+        </div>
+    )
 }
 
 export default Container
